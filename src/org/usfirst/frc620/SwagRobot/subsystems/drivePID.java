4b631b35ffeef750e711db3da5df9c83f60bb199
Azergagash <yusif@sardar.net>
2015-01-23 18:26:38 -0500

    First Commit
    First Commit

---
+++ src/org/usfirst/frc620/SwagRobot/subsystems/drivePID.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.usfirst.frc620.SwagRobot.subsystems;
+
+import edu.wpi.first.wpilibj.AnalogChannel;
+import org.usfirst.frc620.SwagRobot.RobotMap;
+
+/**
+ *
+ * @author Team C
+ */
+public class drivePID {
+        AnalogChannel distanceSensor = RobotMap.drivePIDdistanceSensor;
+
+    
+}
